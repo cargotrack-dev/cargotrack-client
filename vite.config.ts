d4af7653384909cb034463d93583707c2e7d99ce
Idris Alamutu <idris.alamutu@outlook.com>
2025-12-16 21:42:31 +0100

    Fix path aliases for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
 import { defineConfig } from 'vite'
-import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const config: UserConfig = {
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@Core': path.resolve(__dirname, './src/features/Core'),
+      '@UI': path.resolve(__dirname, './src/features/UI'),
     },
   },
-  server: {
-    host: '0.0.0.0',  // Allow connections from all network interfaces
-    port: 3001,       // Use port 3001
-    strictPort: true, // Fail if port is already in use
-  },
-}
-
-export default defineConfig(config)
+})
